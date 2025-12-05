5bff79bdf8ab3f916ca89e9fa1918d6f55cac6a9
Ashika0603 <ashikait06@gmail.com>
2025-12-05 11:09:59 +0530

    initial commit

---
+++ Headervalidation.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pytest
+
+
+@pytest.fixture(scope="session")
+def setup():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+    print("\nWebDriver closed.")
+
+def headerValidation(setup):
+    driver = setup
+    driver.get("https://www.guvi.in/")
+    driver.find_element(By.ID, "login-btn").click()
+    print("navigated to Guvi login page")
+    driver.find_element(By.CSS_SELECTOR, "input[type='email']").send_keys("ashikait06@gmail.com")
+    driver.find_element(By.CSS_SELECTOR, "input[type='password']").send_keys("Positive@25")
+    time.sleep(5)
+    driver.find_element(By.XPATH, "//main[@class='index relative']//div[2]//div[@class='⭐️rwl3jt-0 navbar py-2 px-0']//div[4]//a[text()='Courses']").click()
+    time.sleep(5)
+    print("Login success")
